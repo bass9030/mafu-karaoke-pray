78a349835eea481381b59985faf8b379a3d91dab
bass9030 <bass90301.naver.comm@gmail.com>
2025-04-19 08:43:13 +0900

    트윗 자동전송 구현중

--- app.js
+++ app.js
@@ -26,6 +26,9 @@ async function checkNewSong() {
             }
         }
 
+        /**
+         * @type {import('./getKaraokeNewSong').songInfo[]}
+         */
         let needNotify = [];
 
         if (mafuNewSong.length > 0) {
@@ -41,12 +44,33 @@ async function checkNewSong() {
                 }
             }
         }
+
+        needNotify = [
+            {
+                type: TYPE.TJ,
+                songId: "",
+            },
+        ];
+
         if (needNotify.length > 0) {
             console.log(needNotify);
-            // Tweet.createTweet("");
+            Tweet.createTweet(
+                "신곡 떴나????\n\n" +
+                    needNotify
+                        .map((e) => {
+                            return `[${e.type === TYPE.TJ ? "TJ" : "금영"} | ${
+                                e.songid
+                            }] ${e.title}`;
+                        })
+                        .join("\n")
+            );
         } else {
             let dday = await db.getDDay();
-            console.log(`마후 신곡 노래방 등록 기원 ${dday}일차...`);
+            // console.log(`마후 신곡 노래방 등록 기원 ${dday}일차...`);
+            Tweet.createTweet(
+                `마후마후 신곡 노래방 등록 기원 ${dday}일차\n` +
+                    `#まふまふ #노래방 #신곡 #TJ #금영`
+            );
         }
     } catch (e) {
         console.error(e);
@@ -61,4 +85,13 @@ async function checkNewSong() {
     }
 }
 
+function interval() {
+    let date = new Date();
+    if (date.getHours() == 0 && date.getMinutes() == 0) checkNewSong();
+    setTimeout(interval, 1000 * 60);
+}
+
 checkNewSong();
+interval();
+
+Buffer.byteLength("asdf", "");
--- createTweet.js
+++ createTweet.js
@@ -30,6 +30,7 @@ const features = {
     responsive_web_graphql_timeline_navigation_enabled: true,
     responsive_web_enhance_cards_enabled: false,
 };
+const MAX_LENGTH = 240;
 
 class Twitter {
     constructor(ct0_token, auth_token) {
@@ -37,7 +38,21 @@ class Twitter {
         this.AUTH_TOKEN = auth_token;
     }
 
-    async createTweet(content) {
+    _getBytes(str) {
+        let character;
+        let charBytes = 0;
+
+        for (let i = 0; i < str.length; i += 1) {
+            character = str.charAt(i);
+
+            if (encodeURIComponent(character).length > 4) charBytes += 2;
+            else charBytes += 1;
+        }
+
+        return charBytes;
+    }
+
+    async _createTweet(content) {
         if (this.CT0_TOKEN === undefined) {
             throw new Error("'ct0_token' is undefined");
         }
@@ -79,7 +94,83 @@ class Twitter {
                 method: "POST",
             }
         );
-        console.log(await res.json());
+        return await res.json();
+    }
+
+    async _replyTweet(content, reply) {
+        if (this.CT0_TOKEN === undefined) {
+            throw new Error("'ct0_token' is undefined");
+        }
+        if (this.AUTH_TOKEN === undefined) {
+            throw new Error("'auth_token' is undefined");
+        }
+        let res = await fetch(
+            "https://x.com/i/api/graphql/IVdJU2Vjw2llhmJOAZy9Ow/CreateTweet",
+            {
+                headers: {
+                    accept: "*/*",
+                    "accept-language":
+                        "ko,en-US;q=0.9,en;q=0.8,zh-TW;q=0.7,zh;q=0.6",
+                    authorization:
+                        "Bearer AAAAAAAAAAAAAAAAAAAAANRILgAAAAAAnNwIzUejRCOuH5E6I8xnZz4puTs%3D1Zv7ttfk8LF81IUq16cHjhLTvJu4FA33AGWWjCpTnA",
+                    "content-type": "application/json",
+                    "x-client-transaction-id":
+                        "6+QQD3bLrBYD6fiXpBHIWcctI/n5jSVq+Kqqih0jc8uLA2zJ8L65ZEGoOvxmXo1FQkhERuh2IcA65JlCDvKh+xsUxC8y6A",
+                    "x-client-uuid": "94c60de8-d1bb-42d2-b06d-c39cf87a5725",
+                    "x-twitter-active-user": "yes",
+                    "x-twitter-client-language": "en",
+                    "x-csrf-token": `${this.CT0_TOKEN}`,
+                    cookie: `auth_token=${this.AUTH_TOKEN}; ct0=${this.CT0_TOKEN}`,
+                    Referer: "https://x.com/home",
+                },
+                body: JSON.stringify({
+                    variables: {
+                        tweet_text: content,
+                        dark_request: false,
+                        media: {
+                            media_entities: [],
+                            possibly_sensitive: false,
+                        },
+                        conversation_control: { mode: "Community" },
+                        semantic_annotation_ids: [],
+                        disallowed_reply_options: null,
+                    },
+                    features,
+                }),
+                method: "POST",
+            }
+        );
+        return await res.json();
+    }
+
+    async createTweet(content) {
+        let split_content = content.split("\n");
+        let j = 0;
+
+        for (
+            let i = 0;
+            i < Math.ceil(this._getBytes(content) / MAX_LENGTH);
+            i++
+        ) {
+            let tweet_content = "";
+            for (; j < split_content.length; j++) {
+                let currBytes = this._getBytes(
+                    tweet_content + "\n" + split_content[j]
+                );
+                if (currBytes > MAX_LENGTH) {
+                    break;
+                } else {
+                    tweet_content += "\n" + split_content[j];
+                }
+            }
+            tweet_content = tweet_content.trim();
+            if (tweet_content.length == 0) {
+                console.log("wtf");
+            } else {
+                console.log(tweet_content);
+                console.log("=======");
+            }
+        }
     }
 }
 
